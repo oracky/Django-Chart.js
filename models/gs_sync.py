aea0f5373819a3fc2331c6a0f554aafd9506993c
Michal <michaloracki@wp.pl>
2019-12-20 21:46:22 +0100

    visual changes

--- models/gs_sync.py
+++ models/gs_sync.py
@@ -5,7 +5,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 class GS_data:
     scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
-    credentials = ServiceAccountCredentials.from_json_keyfile_name("PZSP-ef8954300654.json", scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name("models\connect.json", scope)
 
     client = gspread.authorize(credentials)
 
@@ -13,14 +13,17 @@ class GS_data:
 
     data = sheet.get_all_records()   # getting a list of all records
 
-    def get_curr_consumption(self, range=10):
-        return sheet.col_values(1)[-range:]  # getting values from current consumption column (%)
-    def get_out_temp(self, range=10):
-        return sheet.col_values(2)[-range:]  # getting values from temperature outside column (°C)
-    def get_ins_temp(self, range=10):
-        return sheet.col_values(3)[-range:]  # getting values from temperature inside column (°C)
-    def are_wind_open(self, range=10):
-        return sheet.col_values(4)[-range:]  # getting true if windows are open
-    def is_airc_on(self, range=10):
-        return sheet.col_values(5)[-range:]  # getting true if air conditioning launched
+    def __init__(self, range=24):
+        self.range = range
+
+    def get_curr_consumption(self):
+        return list(self.sheet.col_values(1)[-self.range:])  # getting values from current consumption column (%)
+    def get_out_temp(self):
+        return list(self.sheet.col_values(2)[-self.range:])  # getting values from temperature outside column (°C)
+    def get_ins_temp(self):
+        return list(self.sheet.col_values(3)[-self.range:])  # getting values from temperature inside column (°C)
+    def are_wind_open(self):
+        return list(self.sheet.col_values(4)[-self.range:])  # getting true if windows are open
+    def is_airc_on(self):
+        return list(self.sheet.col_values(5)[-self.range:])  # getting true if air conditioning launched
 
--- projekt_zespolowy/views.py
+++ projekt_zespolowy/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import View
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from models import gs_sync
+from models.gs_sync import GS_data
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
@@ -12,20 +12,20 @@ class HomeView(View):
 class ChartData(APIView):
     authentication_classes = []
     permission_classes = []
-    gs_data = gs_sync()
+    gs_data = GS_data()
 
     def get(self, request, format=None):
-        labels = [v for v in range(0,11)]
+        labels = [v for v in range(1, self.gs_data.range+1)]
         consumption = self.gs_data.get_curr_consumption()
         temp_out = self.gs_data.get_out_temp()
         temp_in = self.gs_data.get_ins_temp()
         is_airc_on = self.gs_data.is_airc_on()
 
         data = {
-            "labels" : labels,
-            "consumption" : consumption,
-            "temp_out": temp_out,
-            "temp_in": temp_in,
-            "aircondition": is_airc_on
+            "labels":labels,
+            "consumption":consumption,
+            "temp_out":temp_out,
+            "temp_in":temp_in,
+            "aircondition":is_airc_on
         }
         return Response(data)
